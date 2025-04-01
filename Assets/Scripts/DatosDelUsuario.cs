6fc3213fced38540570936d136d003789f4c353c
Maxistolki <49094305@est.ort.edu.ar>
2025-04-01 09:05:47 -0300

    principio ejercicio 2
    hacer el 2,3 y 4

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string NombreUsuario;
+    public int EdadUsuario;
+    public float AlturaUsuario;
+    public bool EsDonante;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("mi nombre es " + NombreUsuario);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

ab49e886b2f7faad46c8fbd5c1e18daf79c9997d
Maxistolki <49094305@est.ort.edu.ar>
2025-04-08 09:05:09 -0300

    ejerciocios prueba de prueba de prueba
    vamos los if

---
+++ Assets/Scripts/EjemploExamen1/CorversionDeMoneda.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorversionDeMoneda : MonoBehaviour
+{
+    public float monto;
+    public string codigoMoneda;
+    float montoConvertido;
+    float cotizacionMonedaElegida;
+    float montoMinimo = 1000;
+    float cotizacionDolar = 1134.5f;
+    float cotizacionEuro = 1397.16f;
+    float cotizacionReal = 220;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (monto < montoMinimo)
+        {
+            Debug.Log("El monto mínimo es $1000");
+            return;
+        }
+        if (codigoMoneda == "D")
+        {
+            cotizacionMonedaElegida = cotizacionDolar;
+        }
+        else if (codigoMoneda == "E")
+        {
+            cotizacionMonedaElegida = cotizacionEuro;
+        }
+        else if (codigoMoneda == "R")
+        {
+            cotizacionMonedaElegida = cotizacionReal;
+        }
+        else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+            return;
+        }
+        montoConvertido = monto / cotizacionMonedaElegida;
+        Debug.Log("Monto convertido: " + montoConvertido);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
